e95b90612dffab5e2bb2bee275fa17d7d79bf060
brownbeard29 <j.ndakoji@outlook.com>
2016-04-19 14:52:01 +0100

    Exercise 2.2

---
+++ 2.2/hw(2) 2.js
@@ -0,0 +1,17 @@
+
+//EVENT LISTENER
+//var event1 = document.getElementById("button1");
+//event listener whcih invokes the function
+//event1.addEventListener('click', myAlert, false);
+
+function myAlert(a, b, c) {
+    return a + b + c;
+}
+
+document.getElementById("answer").innerHTML = myAlert(1, 2, 3);
+
+
+      
+
+ 
+  
